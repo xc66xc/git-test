31bab39e8627f5a95d08347150642945fe86b722
xcxc <2507849932@qq.com>
2022-08-30 10:34:23 +0800

    hot-fix

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("6666");
         System.out.println("6666222");
         System.out.println("66662221111");
+        System.out.println("66662221111222222");
+        System.out.println("66662221111222222www");
+        System.out.println("hot-fix");
     }
 }
