04b90d13b72038a3bf5a09dcf8ca6b3e1a9c59c5
xcxc <2507849932@qq.com>
2022-08-30 10:24:49 +0800

    first commit12

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("6666");
         System.out.println("6666222");
+        System.out.println("66662221111");
     }
 }
