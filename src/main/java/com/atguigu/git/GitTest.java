f0919431edc364a4c975b0961f1bb59829cd29fc
Jackwang <99536205+xc66xc@users.noreply.github.com>
2022-08-30 11:01:14 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix");
         System.out.println("hot-fix");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix123");
     }
 }
